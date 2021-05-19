a0013254e46503118e340bab5401450d611f42b8
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-05-19 20:53:29 +0200

    Initial commit

---
+++ src/main/java/com/pali/eventgo/EventGoApplication.java
@@ -0,0 +1,13 @@
+package com.pali.eventgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pali/eventgo/EventGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pali.eventgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
