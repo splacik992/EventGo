5a9bb4f46ac06149279d90f6a363114bacae69c8
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-05-20 20:21:45 +0200

    ADD category entity, repository, service

---
+++ src/main/java/com/pali/eventgo/exceptions/ResourceNotExistException.java
@@ -0,0 +1,8 @@
+package com.pali.eventgo.exceptions;
+
+public class ResourceNotExistException extends Exception {
+
+    public ResourceNotExistException(String message) {
+        super(message);
+    }
+}
