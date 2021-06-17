00d02c45441ce29ac2cf097164d32ac17cd4b295
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-06-17 23:36:55 +0200

    ADD remove creating Event in test.

--- src/test/java/com/pali/eventgo/repository/EventRepositoryTest.java
+++ src/test/java/com/pali/eventgo/repository/EventRepositoryTest.java
@@ -1,21 +1,17 @@
 package com.pali.eventgo.repository;
 
 import com.pali.eventgo.entity.Event;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EventRepositoryTest {
 
     @Autowired
@@ -27,17 +23,9 @@ public class EventRepositoryTest {
 
     @Test
     public void findByName() {
-        Event event = new Event.EventBuilder()
-                .buildId(1L)
-                .buildName("Event Name")
-                .buildDescription("bla bla")
-                .build();
-
-        eventRepository.save(event);
-
         Event findByName = eventRepository.findByName("Event Name");
-        assertEquals("Event Name", findByName.getName());
 
+        assertEquals("Event Name", findByName.getName());
 
     }
 }
