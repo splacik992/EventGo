9a93b8d61c3e8e767df59c452db1efc56343d642
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-06-18 18:33:19 +0200

    Optimize imports in IndexController and create new query in 'EventRepository.findEventById'

--- src/main/java/com/pali/eventgo/repository/EventRepository.java
+++ src/main/java/com/pali/eventgo/repository/EventRepository.java
@@ -2,6 +2,8 @@ package com.pali.eventgo.repository;
 
 import com.pali.eventgo.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     Event findByName(String eventName);
     List<Event> findAllByOrderByIdDesc();
+
+    @Query(value = "SELECT * FROM event where id = :eventId", nativeQuery = true)
+    Event findEventById(@Param("eventId") Long id);
+
 }
