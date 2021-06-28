a348a102337e548c094667a51a2e9583ebe3cb48
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-06-28 23:45:07 +0200

    ADD findUserByEmail method and create method where hashCode is set to User

--- src/main/java/com/pali/eventgo/services/UserServiceImpl.java
+++ src/main/java/com/pali/eventgo/services/UserServiceImpl.java
@@ -2,8 +2,10 @@ package com.pali.eventgo.services;
 
 import com.pali.eventgo.entity.AppUser;
 import com.pali.eventgo.entity.Role;
+import com.pali.eventgo.exceptions.ResourceNotExistException;
 import com.pali.eventgo.repository.RoleRepository;
 import com.pali.eventgo.repository.UserRepository;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final static String USER_NOT_FOUND = "User z takim email nie istnieje";
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
@@ -24,18 +28,33 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
     }
+
     @Override
     public AppUser findByUserName(String username) {
         return userRepository.findByUsername(username);
     }
 
+    @Override
+    public AppUser findUserByEmail(String email) {
+        AppUser appUser = userRepository.findAppUserByEmail(email);
+        if (appUser == null) {
+            throw new ResourceNotExistException(USER_NOT_FOUND);
+        } else
+            return userRepository.findAppUserByEmail(email);
+    }
+
+    @Override
+    public AppUser updateUser(AppUser appUser) {
+        return userRepository.save(appUser);
+    }
 
     @Override
     public void saveUser(AppUser appUser) {
         appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
-        appUser.setAccountEnabled(1);
         Role userRole = roleRepository.findByName("ROLE_USER");
         appUser.setRoles(new HashSet<>(Collections.singletonList(userRole)));
+        appUser.setHashCodeToEnableAccount(
+                givenUsingApache_whenGeneratingRandomAlphanumericString_thenCorrect());
         userRepository.save(appUser);
     }
 
@@ -43,4 +62,10 @@ public class UserServiceImpl implements UserService {
     public List<AppUser> findAll() {
         return userRepository.findAll();
     }
+
+
+    public String givenUsingApache_whenGeneratingRandomAlphanumericString_thenCorrect() {
+        return RandomStringUtils.randomAlphanumeric(100);
+    }
+
 }
