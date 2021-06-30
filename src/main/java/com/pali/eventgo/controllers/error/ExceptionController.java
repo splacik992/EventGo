63776594fc1acfa323082ed608d0cec53d984393
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-06-30 19:16:48 +0200

    modify global exception handler (create methods with handling NumberFormatException and UsernameNotFoundException)

--- src/main/java/com/pali/eventgo/controllers/error/ExceptionController.java
+++ src/main/java/com/pali/eventgo/controllers/error/ExceptionController.java
@@ -2,8 +2,12 @@ package com.pali.eventgo.controllers.error;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,6 +20,32 @@ public class ExceptionController {
     public String handleError(HttpServletRequest req, Exception exception){
         logger.error("Request" + req.getRequestURL() + " raised " + exception);
         return "error";
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleAlreadyExist(Exception exception){
+        logger.error("Handling Number Format Exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ModelAndView handleUserNotFoundException(Exception exception){
+        logger.error("Handling UserNotFoundException");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 
 
