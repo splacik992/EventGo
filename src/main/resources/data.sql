cfbeccd2c9cc171ca8d9d62652759fb6f0882d2a
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-06-30 18:41:08 +0200

    insert into data.sql roles like USER and ADMIN

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ INSERT INTO categories (id,name ) VALUES (2,'Siatkówka');
 INSERT INTO categories (id,name ) VALUES (3,'Bilard');
 INSERT INTO categories (id,name ) VALUES (4,'Kręgle');
 INSERT INTO categories (id,name ) VALUES (5,'Rower');
+
+INSERT INTO role (id, name) VALUES (1,'ROLE_USER');
+INSERT INTO role (id, name) VALUES (2,'ROLE_ADMIN');
